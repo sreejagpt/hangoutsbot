481bf7901c6ddb763e570cc7ac43a5184ddf7fdc
roo2 <roo2@protonmail.com>
2017-09-21 19:55:21 +1000

    make sexy

--- hangupsbot/plugins/toastfact.py
+++ hangupsbot/plugins/toastfact.py
@@ -1,13 +1,25 @@
 import asyncio
 from random import shuffle
+# vim: set ts=4 expandtab sw=4
 
 import plugins
+from hangups import ChatMessageSegment
 
+import aiohttp
+import io
+import json
+import os.path
+import re
+import urllib.parse
+import plugins
+
+import aiohttp, logging, io, os, re
+logger = logging.getLogger(__name__)
 
 def _initialise(bot):
     plugins.register_user_command(["toastfact"])
 
-
+@asyncio.coroutine
 def toastfact(bot, event):
     """
     /bot toastfact
@@ -30,5 +42,73 @@ def toastfact(bot, event):
     ]
 
     shuffle(answers)
+    image_id = yield from image_validate_and_upload_single("https://i.imgur.com/yIe47Xw.gif", bot)
+    yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
 
-    bot.send_message(event.conv, answers[0])
+    yield from bot.coro_send_message(event.conv, answers[0] )
+
+
+
+def image_validate_link(image_uri, reject_googleusercontent=True):
+    """
+    validate and possibly mangle supplied image link
+    returns False, if not an image link
+            <string image uri>
+    """
+
+    if " " in image_uri:
+        """immediately reject anything with non url-encoded spaces (%20)"""
+        return False
+
+    probable_image_link = False
+
+    image_uri_lower = image_uri.lower()
+
+    if re.match("^(https?://)?([a-z0-9.]*?\.)?imgur.com/", image_uri_lower, re.IGNORECASE):
+        """imgur links can be supplied with/without protocol and extension"""
+        probable_image_link = True
+
+    elif image_uri_lower.startswith(("http://", "https://", "//")) and image_uri_lower.endswith((".png", ".gif", ".gifv", ".jpg", ".jpeg")):
+        """other image links must have protocol and end with valid extension"""
+        probable_image_link = True
+
+    if probable_image_link and reject_googleusercontent and ".googleusercontent." in image_uri_lower:
+        """reject links posted by google to prevent endless attachment loop"""
+        logger.debug("rejected link {} with googleusercontent".format(image_uri))
+        return False
+
+    if probable_image_link:
+
+        """special handler for imgur links"""
+        if "imgur.com" in image_uri:
+            if not image_uri.endswith((".jpg", ".gif", "gifv", "webm", "png")):
+                image_uri = image_uri + ".gif"
+            image_uri = "https://i.imgur.com/" + os.path.basename(image_uri)
+
+            """imgur wraps animations in player, force the actual image resource"""
+            image_uri = image_uri.replace(".webm",".gif")
+            image_uri = image_uri.replace(".gifv",".gif")
+
+        logger.debug('{} seems to be a valid image link'.format(image_uri))
+
+        return image_uri
+
+    return False
+
+@asyncio.coroutine
+def image_upload_single(image_uri, bot):
+    logger.info("getting {}".format(image_uri))
+    filename = os.path.basename(image_uri)
+    r = yield from aiohttp.request('get', image_uri)
+    raw = yield from r.read()
+    image_data = io.BytesIO(raw)
+    image_id = yield from bot._client.upload_image(image_data, filename=filename)
+    return image_id
+
+@asyncio.coroutine
+def image_validate_and_upload_single(text, bot, reject_googleusercontent=True):
+    image_id = False
+    image_link = image_validate_link(text, reject_googleusercontent=reject_googleusercontent)
+    if image_link:
+        image_id = yield from image_upload_single(image_link, bot)
+    return image_id
